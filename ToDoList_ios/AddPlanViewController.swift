52a146b53acbe3e18eaadcc9c6b5711332d2e9bb
yuki-325 <sakeikura325@yahoo.co.jp>
2021-03-15 21:26:24 +0900

    [修正]: HomeViewController.swift 画面をスクロールしてもfloatActionBtnが消えないように修正しました。

---
+++ ToDoList_ios/AddPlanViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPlanViewController.swift
+//  ToDoList_ios
+//
+//  Created by 中野勇貴 on 2021/03/09.
+//
+
+import UIKit
+
+class AddPlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDoList_ios/HomeViewController.swift
+++ ToDoList_ios/HomeViewController.swift
@@ -9,49 +9,18 @@ import UIKit
 import MaterialComponents.MaterialButtons
 
 class HomeViewController: UIViewController {
-    
-    
-    
+
     @IBOutlet weak var toDoTableView: UITableView!
     @IBOutlet weak var floatActionBtn: MDCFloatingButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         toDoTableView.delegate = self
         toDoTableView.dataSource = self
         toDoTableView.register(UINib(nibName: C.toDoCellNibName, bundle: nil), forCellReuseIdentifier: C.toDoCellIdentifier)
-        
         floatActionBtn.inkColor = UIColor(named: C.Colors.dark)
         floatActionBtn.inkStyle = .bounded
     }
-    
-    // スクロール中に呼ばれる
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // 表示・非表示を綺麗に見せるために、アニメーションを適用
-        UIView.animate(withDuration: 0.25) {
-            self.floatActionBtn.alpha = 0
-        }
-    }
-    
-    // スクロールが終わった時に呼ばれる
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        UIView.animate(withDuration: 0.25) {
-            self.floatActionBtn.alpha = 1
-        }
-    }
-    
-    // ドラッグスクロールが終わった時に呼ばれる
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        UIView.animate(withDuration: 0.25) {
-            self.floatActionBtn.alpha = 1
-        }
-    }
-    
-    @IBAction func floatActionBtnPressed(_ sender: Any) {
-        
-    }
-    
 }
 
 
